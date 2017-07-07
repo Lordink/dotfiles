aa67b03ee0b3c9ec0eda3303006ecabe4da3d575
Petr Dyachikhin <petr.dyachikhin@arction.com>
2017-07-07 20:25:38 +0300

    Termite config added

--- setup.sh
+++ setup.sh
@@ -2,8 +2,10 @@ ln -s $PWD/xmInit.sh ~
 ln -s $PWD/colortest.sh ~
 mkdir --parents ~/.xmonad
 mkdir --parents ~/.config
+mkdir --parents ~/.config/termite
 ln -s $PWD/xmonad.hs ~/.xmonad
 ln -s $PWD/compton.conf ~/.config
+ln -s $PWD/termiteConfig ~/.config/termite/config
 ln -s $PWD/.[!.]* ~
 rm ~/.git -rf
 #mkdir --parents ~/.xmonad/ && mv ./.xmonad/xmonad.hs ~/.xmonad
